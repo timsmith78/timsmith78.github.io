8e1a96bdadc05576de473910cd5d9881dddc88c6
Timothy Smith <timsmith78@yahoo.com>
2018-06-21 02:23:09 -0500

    Added RPS buttons and fixed bugs

--- rps-game/assets/javascript/rps.js
+++ rps-game/assets/javascript/rps.js
@@ -11,12 +11,19 @@ firebase.initializeApp(config);
 
 var rpsDb = firebase.database();
 var myPlayerID = 0
+var myPlayerData = {
+    name: "",
+    wins: 0,
+    losses: 0,
+    lastPick: ""
+}
 var player1taken = false
+
 
 
 // Handle database errors
 function dbError(error) {
-    if (error) $("#score").text("Database read failed.  Error code: " + error.code)
+    if (error) $("#score").text("Database access failed.  Error code: " + error.code)
 }
 
 // Gather players
@@ -29,16 +36,18 @@ rpsDb.ref("/players").on("value", (snapshot) => {
 
     console.log("currNumPlayers: " + currNumPlayers)
     if (currNumPlayers > 0) {
-        snapshot.forEach( (childSnapshot) => {
+        snapshot.forEach((childSnapshot) => {
             let playerNum = childSnapshot.key
             player1taken = (playerNum == 1)
             console.log("player1taken: " + player1taken)
-            playersArea.append($("<h5>").text(childSnapshot.val().name + ", you are player " + playerNum))
+            if (playerNum == myPlayerID) {
+                playersArea.append($("<h5>").text(childSnapshot.val().name + ", you are player " + playerNum))
+            }
             $("#player-" + playerNum).text("Player " + playerNum + ": " + childSnapshot.val().name)
-            
+
         })
     }
-  
+
     // Check to see if we can accept a new player
     if (currNumPlayers < 2 && myPlayerID === 0) {
         // If so, display new player form...
@@ -58,24 +67,64 @@ rpsDb.ref("/players").on("value", (snapshot) => {
         form.append(inputName)
         form.append(submitButton)
         playersArea.append(form)
-        // ...and add new player to database
-        submitButton.click( (evt) => {
+        // ...clear the turn tracker...
+        rpsDb.ref("/turn").set({
+            player: 0
+        })
+        // ...and add the new player to the database
+        submitButton.click((evt) => {
             evt.preventDefault();
             let newName = $("#new-player").val();
             myPlayerID = player1taken ? 2 : 1
-            
+
             console.log("myPlayerID: " + myPlayerID)
-            rpsDb.ref("/players").child(myPlayerID).set({
-                name: newName,
-                wins: 0,
-                losses: 0,
-                lastPick: ""
-            })
+            myPlayerData.name = newName;
+            rpsDb.ref("/players").child(myPlayerID).set(myPlayerData)
             rpsDb.ref("/players").child(myPlayerID).onDisconnect().remove(dbError);
+            // If we have both players present, start the game
+            if (currNumPlayers === 1) {
+                rpsDb.ref("/turn").set({
+                    player: 1
+                })
+            }
+
         })
     }
+
 }, dbError)
 
+rpsDb.ref("/turn").on("value", (turnSS) => {
+    let currTurn = turnSS.val().player
 
-$("#player-1").text("Waiting for player 1 to join...")
-$("#player-2").text("Waiting for player 2 to join...")
+    if (currTurn === 0) {
+        return
+    } else if (currTurn === 3) {
+        // Display result
+    } else if (currTurn === myPlayerID) {
+        // If it's my turn, let me pick rock, paper, or scissors
+        let myPlayerDiv = $("#player-" + currTurn)
+        myPlayerDiv.append($("<p>").text("Your turn"))
+        myPlayerDiv.append($("<button>").text("Rock")
+            .attr("class", "btn btn-secondary m-1")
+            .attr("id", "rock"))
+        myPlayerDiv.append($("<button>").text("Paper")
+            .attr("class", "btn btn-info m-1")
+            .attr("id", "paper"))
+        myPlayerDiv.append($("<button>").text("Scissors")
+            .attr("class", "btn btn-primary m-1")
+            .attr("id", "scissors"))
+        $(".btn").click( (rpsClick) => {
+            myPlayerData.lastPick = rpsClick.target.getAttribute("id")
+            rpsDb.ref("/players").child(myPlayerID).update(myPlayerData)
+            rpsDb.ref("/turn").update({
+                player: ++currTurn
+            })
+        })
+
+
+    } else {
+        // It is my opponents turn, so I'm waiting...
+        let myPlayerDiv = $("#player-" + currTurn)
+        myPlayerDiv.append($("<p>").text("Waiting for my opponent to play..."))
+    }
+}, dbError)
